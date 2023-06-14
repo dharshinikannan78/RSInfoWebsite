97a475e17cce9c4c573ccc719c4f703667a1f126
salmank07 <mohamedsalmankhan509@gmail.com>
2023-06-15 01:27:40 +0530

    services and about finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,23 @@ export class AppComponent implements OnInit {
   title = 'RS INFOSOLUTION';
   ariaExpanded: boolean = false;
   windowScrolled: boolean = false;
-  constructor() { AOS.init({ duration: 1000 });}
+  serviceData: any;
+  constructor() {
+    AOS.init({ duration: 1000 });
+
+    this.serviceData = [
+      { name: "Data Annotation", src: "top-cat3.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "Accessibility Service", src: "top-cat2.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "Typesetting", src: "top-cat1.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "Digital Publishing", src: "top-cat4.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "Data Labeling", src: "top-cat5.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "Website Development", src: "top-cat6.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "Web Application Development", src: "top-cat2.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "Mobile App Development", src: "top-cat5.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+      { name: "E-Books", src: "top-cat1.svg", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ea dolor quaerat" },
+    ]
+
+  }
   @HostListener('window:scroll', ['$event'])
   ngOnInit(): void {
 
